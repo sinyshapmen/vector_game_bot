81b1374f297bec0df0679559bdf983ad7f7632c1
UltraGeoPro <120571667+Ultrageopro1966@users.noreply.github.com>
2023-12-09 23:58:37 +0300

    Update queue_bot.py

--- queue_bot.py
+++ queue_bot.py
@@ -3,34 +3,43 @@ from queue import Queue
 import time
 
 # Определите глобальную переменную для очереди запросов
-request_queue = Queue()
+request_queue = []
 
 
 # Функция для добавления запроса в очередь
-def add_request_to_queue(answer: str, group_id: str, chat_id: str, full_name: str):
-    request_queue.put((answer, group_id, chat_id, full_name))
+def add_request_to_queue(
+    answer: str,
+    group_id: str,
+    chat_id: str,
+    full_name: str,
+    message_queue_id: str,
+    user_id: str,
+):
+    request_queue.append(
+        (answer, group_id, chat_id, full_name, message_queue_id, user_id)
+    )
 
 
 # Функция для обработки запросов из очереди
 def process_requests(f, logger, delay):
     while True:
-        time.sleep(60)
+        time.sleep(delay)
 
         # Получаем запрос из очереди
-        request = request_queue.get()
-        if request is None:
-            break  # Завершаем цикл при получении None из очереди
-        answer, group_id, chat_id, full_name = request
+        if len(request_queue):
+            request = request_queue[0]
+            if request is None:
+                break  # Завершаем цикл при получении None из очереди
+            answer, group_id, chat_id, full_name, message_queue_id, user_id = request
 
-        f(request)
-        print(f"Processing request: {answer}, {group_id}, {chat_id}, {full_name}")
+            f(request)
 
-        if logger is not None:
-            logger.info(
-                f"Processing request: {answer}, {group_id}, {chat_id}, {full_name}"
-            )
+            if logger is not None:
+                logger.info(
+                    f"Processing request: {answer}, {group_id}, {chat_id}, {full_name}, {message_queue_id}, {user_id}"
+                )
 
-        request_queue.task_done()
+            request_queue.pop(0)
 
 
 def start_thread(f, logger=None, delay=60):
@@ -39,4 +48,4 @@ def start_thread(f, logger=None, delay=60):
 
 
 def get_queue_length():
-    return request_queue.qsize()
+    return len(request_queue)
