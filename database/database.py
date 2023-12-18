a57970b0dbbc1fb36ccf6b7a5c28995e365fed02
FoxFil <filipp.vesker@gmail.com>
2023-12-18 16:55:18 +0300

    new log system

--- database/database.py
+++ database/database.py
@@ -18,23 +18,43 @@ class PostgreClient:
             if logger is not None:
                 self.logger.error(f"ERROR: {e}")
 
-    def init_user_table(self):
-        try:
-            self.__cur.execute(
-                """
-            CREATE TABLE IF NOT EXISTS users (
-                user_id INTEGER PRIMARY KEY,
-                timestamp_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                credits INTEGER DEFAULT 0
-            );
-            """
-            )
-            if self.logger is not None:
-                self.logger.info(f"table users initiaized")
-        except Exception as e:
-            if self.logger is not None:
-                self.logger.error(f"ERROR: {e}")
+    def log_decorator(message: str):
+        def actual_decorator(func):
+            def inner(self, *args, **kwargs):
+                try:
+                    if self.logger is not None:
+                        args_str = ", ".join(str(arg) for arg in args)
+                        kwargs_str = ", ".join(
+                            f"{key}={value}" for key, value in kwargs.items()
+                        )
+                        full_message = (
+                            f"{message} - Args: [{args_str}], Kwargs: {{{kwargs_str}}}"
+                        )
+                        self.logger.info(full_message)
+                    result = func(self, *args, **kwargs)
+                except Exception as e:
+                    if self.logger is not None:
+                        self.logger.error(f"ERROR: {e}")
+                    return None
+                return result
 
+            return inner
+
+        return actual_decorator
+
+    @log_decorator("table initialized")
+    def init_user_table(self):
+        self.__cur.execute(
+            """
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            timestamp_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            credits INTEGER DEFAULT 0
+        );
+        """
+        )
+
+    @log_decorator("Successful interaction 'add_user_if_not_exists'")
     def add_user_if_not_exists(self, user_id):
         self.__cur.execute("SELECT COUNT(*) FROM users WHERE user_id = %s", (user_id,))
         count = self.__cur.fetchone()[0]
@@ -45,54 +65,25 @@ class PostgreClient:
                 (user_id,),
             )
             self.__conn.commit()
-            if self.logger is not None:
-                self.logger.info(f"User ID {user_id} added")
-        else:
-            if self.logger is not None:
-                self.logger.info(f"User ID {user_id} alredy exists.")
 
+    @log_decorator("Successful interaction 'get_user_string_by_id'")
     def get_user_string_by_id(self, user_id):
-        try:
-            self.__cur.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
-            user_string = self.__cur.fetchone()
-            if user_string:
-                return user_string
-            else:
-                if self.logger is not None:
-                    self.logger.info(f"No user found with ID {user_id}")
-                return None
-        except Exception as e:
-            if self.logger is not None:
-                self.logger.error(f"ERROR: {e}")
+        self.__cur.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+        user_string = self.__cur.fetchone()
+        if user_string:
+            return user_string
+        else:
             return None
 
+    @log_decorator("Successful interaction 'add_credits_to_user'")
     def add_credits_to_user(self, user_id, credits_to_add):
-        try:
-            self.__cur.execute(
-                "UPDATE users SET credits = credits + %s WHERE user_id = %s",
-                (credits_to_add, user_id),
-            )
-            self.__conn.commit()
-            if self.logger is not None:
-                self.logger.info(f"{credits_to_add} credits added to User ID {user_id}")
-        except Exception as e:
-            if self.logger is not None:
-                self.logger.error(f"ERROR: {e}")
+        self.__cur.execute(
+            "UPDATE users SET credits = credits + %s WHERE user_id = %s",
+            (credits_to_add, user_id),
+        )
+        self.__conn.commit()
 
+    @log_decorator("Successful interaction 'drop_table'")
     def drop_table(self, table_name):
-        try:
-            self.__cur.execute(f"DROP TABLE IF EXISTS {table_name};")
-            self.__conn.commit()
-            if self.logger is not None:
-                self.logger.info(f"Table {table_name} dropped")
-        except Exception as e:
-            if self.logger is not None:
-                self.logger.error(f"ERROR: {e}")
-
-
-client = PostgreClient(
-    host="84.23.52.41",
-    dbname="user_db",
-    password="zVA89Y31(rN91K9c6",
-    user="korney",
-)
+        self.__cur.execute(f"DROP TABLE IF EXISTS {table_name};")
+        self.__conn.commit()
